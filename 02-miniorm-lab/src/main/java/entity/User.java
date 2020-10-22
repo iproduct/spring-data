dd193e0165340bce8a9e96c0e7196d07f2b07af6
Trayan Iliev <office@iproduct.org>
2020-10-22 20:36:47 +0300

    User created

---
+++ 02-miniorm-lab/src/main/java/entity/User.java
@@ -0,0 +1,86 @@
+package entity;
+
+import java.util.Objects;
+
+public class User {
+    private int id;
+    private String username;
+    private String password;
+    private int age;
+    private String registrationDate;
+
+    public User() {
+    }
+
+    public User(String username, String password, int age, String registrationDate) {
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.registrationDate = registrationDate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(String registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("User{");
+        sb.append("id=").append(id);
+        sb.append(", username='").append(username).append('\'');
+        sb.append(", password='").append(password).append('\'');
+        sb.append(", age=").append(age);
+        sb.append(", registrationDate='").append(registrationDate).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
