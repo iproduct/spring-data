7679d528b7d09e8d52543a4d7089fd917602d742
Trayan Iliev <office@iproduct.org>
2020-10-22 21:14:04 +0300

    ORM mapping added to User entity

--- 02-miniorm-lab/src/main/java/entity/User.java
+++ 02-miniorm-lab/src/main/java/entity/User.java
@@ -1,13 +1,23 @@
 package entity;
 
+import orm.annotation.Column;
+import orm.annotation.Entity;
+import orm.annotation.Id;
+
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Entity(name="USERS")
 public class User {
+    @Id
     private int id;
+    @Column(name="username")
     private String username;
+    @Column(name = "password")
     private String password;
+    @Column(name = "age")
     private int age;
+    @Column(name = "registration_rate")
     private LocalDate registrationDate;
 
     public User() {
