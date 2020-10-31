50f313f1003636d8c3858585503ea9c62c9aec5d
Trayan Iliev <office@iproduct.org>
2020-10-31 17:34:44 +0200

    rest mvc hello endpoint added

---
+++ 05-restmvc-demo-lab/src/main/java/course/springdata/restmvc/web/HelloController.java
@@ -0,0 +1,13 @@
+package course.springdata.restmvc.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Spring MVC!";
+    }
+}
