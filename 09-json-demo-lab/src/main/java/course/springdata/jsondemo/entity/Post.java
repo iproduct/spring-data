d98654b59a697b08ea0224c439397c6d4fb37ada
Trayan Iliev <office@iproduct.org>
2020-11-12 21:13:43 +0200

    POST method addPost created with 201 Created and URI bulder for Location

--- 09-json-demo-lab/src/main/java/course/springdata/jsondemo/entity/Post.java
+++ 09-json-demo-lab/src/main/java/course/springdata/jsondemo/entity/Post.java
@@ -1,5 +1,6 @@
 package course.springdata.jsondemo.entity;
 
+import com.google.gson.annotations.Expose;
 import com.sun.istack.NotNull;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
@@ -18,32 +19,39 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Post {
+    @Expose
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
+    @Expose
     @NonNull
     @NotNull
     @Length(min=3, max=80, message = "Title must be minimum 3 and maximum 80 characters long.")
     private String title;
 
+    @Expose
     @NonNull
     @NotNull
     @Length(min=3, max=2048)
     private String content;
 
+    @Expose
     @NonNull
     @NotNull
     @URL
     private String imageUrl;
 
+    @Expose
     @NonNull
     @NotNull
     @Length(min=3, max=80, message = "Author is required property (between 3 and 80 characters)")
     private String author;
 
+//    @Expose
     private LocalDateTime created = LocalDateTime.now();
+//    @Expose
     private LocalDateTime modified = LocalDateTime.now();
 
 }
--- 09-json-demo-lab/src/main/java/course/springdata/jsondemo/web/GsonPostController.java
+++ 09-json-demo-lab/src/main/java/course/springdata/jsondemo/web/GsonPostController.java
@@ -3,17 +3,19 @@ package course.springdata.jsondemo.web;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import course.springdata.jsondemo.dao.PostRepository;
+import course.springdata.jsondemo.entity.Post;
 import course.springdata.jsondemo.service.PostService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.annotation.PostConstruct;
 
 @RestController
-@RequestMapping("/api/posts")
+@RequestMapping("/api/gson/posts")
+@Slf4j
 public class GsonPostController {
     @Autowired
     private PostService postService;
@@ -26,5 +28,19 @@ public class GsonPostController {
     @GetMapping
     public String getPosts() {
         return gson.toJson(postService.getAllPosts());
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addPost(@RequestBody String body) {
+        log.info("Body received: {}", body);
+        Post post = gson.fromJson(body, Post.class);
+        log.info("Post: {}", post);
+        Post created = postService.addPost(post);
+        return ResponseEntity.created(
+                ServletUriComponentsBuilder.fromCurrentRequest()
+                    .pathSegment("{id}")
+                    .buildAndExpand(created.getId().toString())
+                    .toUri()
+        ).body(gson.toJson(created));
     }
 }
