8a26797a020ce86a773b98708609c0370b26bde4
Trayan Iliev <office@iproduct.org>
2020-11-16 22:48:03 +0200

    XML Schema Definition generated using maven plugin + unmarshalling validation

--- 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/Main.java
+++ 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/Main.java
@@ -4,13 +4,18 @@ import course.springdata.xmldemo.model.Address;
 import course.springdata.xmldemo.model.Person;
 import course.springdata.xmldemo.model.Persons;
 import course.springdata.xmldemo.model.PhoneNumber;
+import org.xml.sax.SAXException;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import javax.xml.XMLConstants;
+import javax.xml.bind.*;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,10 +42,40 @@ public class Main {
             marshaller.marshal(persons.get(0), new File("person.xml"));
 //            marshaller.marshal(persons.get(0), System.out);
             //4. Marshal multiple persons to persons.xml
-            marshaller.marshal(new Persons(persons), new File("persons.xml"));
-            marshaller.marshal(new Persons(persons), System.out);
+//            marshaller.marshal(new Persons(persons), new File("persons.xml"));
+//            StringWriter out  = new StringWriter();
+//            marshaller.marshal(new Persons(persons), out);
+//            System.out.printf("StringWriter: %s%n", out.toString());
 
+            //5. Unmarshal multiple Persons from XML to Java
+            Unmarshaller unmarshaller = ctx.createUnmarshaller();
+
+            //6. Set validation
+            SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = sf.newSchema(new File("persons.xsd"));
+            unmarshaller.setSchema(schema);
+            unmarshaller.setEventHandler(
+                    (ValidationEvent ve) -> {
+                        if(ve.getSeverity() != ValidationEvent.WARNING) {
+                            System.out.printf("%s: Line:Col[%d:%d] - %s%n",
+                                    ve.getSeverity(),
+                                    ve.getLocator().getLineNumber(), ve.getLocator().getColumnNumber(),
+                                    ve.getMessage()
+                            );
+                        }
+                        return true;
+                    }
+            );
+            Persons unmarshalled = (Persons) unmarshaller.unmarshal(new File("persons.xml"));
+            unmarshalled.getPersons().forEach(p ->
+                    System.out.printf("| %5d | %-15.15s | %-15.15s | %-40.40s | %-40.40s |%n",
+                            p.getId(), p.getFirstName(), p.getLastName(),
+                            p.getAddress().getCountry() + ", " + p.getAddress().getCity() + ", " + p.getAddress().getStreet(),
+                            p.getPhoneNumbers().stream().map(PhoneNumber::getNumber)
+                                    .collect(Collectors.joining(", "))));
         } catch (JAXBException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
             e.printStackTrace();
         }
 
--- 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/model/Address.java
+++ 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/model/Address.java
@@ -2,10 +2,7 @@ package course.springdata.xmldemo.model;
 
 import lombok.*;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.*;
 
 @XmlAccessorType(XmlAccessType.FIELD)
 @Data
@@ -16,8 +13,10 @@ public class Address {
     @XmlAttribute(required = true)
     private Long id;
     @NonNull
+    @XmlElement(required = true)
     private String country;
     @NonNull
+    @XmlElement(required = true)
     private String city;
     @NonNull
     private String street;
