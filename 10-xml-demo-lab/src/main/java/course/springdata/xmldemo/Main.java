4fa826ecc8169a436dc37ac67a2cf5ed47907f20
Trayan Iliev <office@iproduct.org>
2020-11-16 21:59:40 +0200

    multiple Persons data prepared

--- 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/Main.java
+++ 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/Main.java
@@ -8,13 +8,23 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.File;
+import java.util.List;
 import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Address address1 = new Address(1L, "Bulgaria", "Sofia", "bul. Tzar Osvoboditel, 50");
-        Person person1 = new Person(1L, "Petar", "Petrov", address1,
-                Set.of(new PhoneNumber("+359 885 123456"), new PhoneNumber("+359 2 9731425")));
+        List<Address> addresses = List.of(
+                new Address(1L, "Bulgaria", "Sofia", "bul. Tzar Osvoboditel, 50"),
+                new Address(1L, "Bulgaria", "Plovdiv", "ul. Gladston, 17")
+        );
+        List<Person> persons = List.of(
+                new Person(1L, "Petar", "Petrov", addresses.get(0),
+                        Set.of(new PhoneNumber("+359 885 123456"), new PhoneNumber("+359 2 9731425"))),
+                new Person(1L, "Georgi", "Hristov", addresses.get(1),
+                        Set.of(new PhoneNumber("+359 887 456677"), new PhoneNumber("+359 32 654237"))),
+                new Person(1L, "Stamat", "Petrov", addresses.get(0),
+                        Set.of(new PhoneNumber("+359 889 567895"), new PhoneNumber("+359 2 9731425")))
+        );
 
         // 1. Create JAXBContext
         try {
@@ -23,8 +33,8 @@ public class Main {
             Marshaller marshaller = ctx.createMarshaller();
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
             //3. Marshal POJO to XML
-            marshaller.marshal(person1, new File("person.xml"));
-            marshaller.marshal(person1, System.out);
+            marshaller.marshal(persons.get(0), new File("person.xml"));
+            marshaller.marshal(persons.get(0), System.out);
 
         } catch (JAXBException e) {
             e.printStackTrace();
