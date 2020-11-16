dc7b809ba0d11297d44f6d6fd42cfbca4c9ba9e9
Trayan Iliev <office@iproduct.org>
2020-11-16 21:39:22 +0200

    person model created

--- 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/model/Address.java
+++ 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/model/Address.java
@@ -1,5 +1,25 @@
 package course.springdata.xmldemo.model;
 
+import lombok.*;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class Address {
+    @XmlAttribute(required = true)
+    private Long id;
+    @NonNull
+    private String country;
+    @NonNull
+    private String city;
+    @NonNull
+    private String street;
 
 }
--- 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/model/PhoneNumber.java
+++ 10-xml-demo-lab/src/main/java/course/springdata/xmldemo/model/PhoneNumber.java
@@ -1,4 +1,22 @@
 package course.springdata.xmldemo.model;
 
+import lombok.*;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class PhoneNumber {
+    private static long nextId = 0;
+    @XmlAttribute(required = true)
+    private Long id = ++ nextId;
+    @NonNull
+    private String number;
+
 }
